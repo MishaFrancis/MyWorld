89c9f1b60fd4bd093e03a3fa8e8e7e6e19b3ae9e
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-02-26 23:12:15 +0100

    asdf

--- test_sample.py
+++ test_sample.py
@@ -3,15 +3,9 @@ import site
 
 class test_regression:
     @pytest.mark.smoke
-<<<<<<< HEAD
-    def test_01():
-        print("+++++++++++++sdsd++")
-        assert 1 + 1 == 2
-=======
     def test_01():
         print("++++++++++++++++++++++++++++++++++++++++++++++++")
         assert 1 + 1 == 2
->>>>>>> ff8e9073407197ed88c926494e4ccb3366f6b7e9
 
 
 if __name__ == '__main__':
