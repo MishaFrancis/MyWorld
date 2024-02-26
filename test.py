6288ceab56d0e86625323070b849f43c9a0e115d
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-02-26 18:13:53 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import pytest
 
 class test_regression:
-    def test_01():
+    @pytest.mark.smoke
+    def test_01(self):
         print("++++++++++++++++++++++++++++++++++++++++++++++++")
         assert 1 + 1 == 5
+
+
+if __name__ == '__main__':
+    test_regression.test_01()
