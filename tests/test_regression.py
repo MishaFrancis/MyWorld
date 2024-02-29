045ae16c07e390dcdb6b1c5ada614f213fc6be95
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-02-29 23:10:36 +0100

    zxcvzxcv

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,10 +1,10 @@
 import pytest
 
 class Test_regression:
-    def test_01():
+    def test_01(self):
         print("+++++++++++++++++++++++++++++++++++")
 
-    def test_02():
+    def test_02(self):
         assert 5 == 5
         print("=========333===d=======")
 
