d5508bb09aa58526052ea55f3277a70e1519e516
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-02-29 21:50:30 +0100

    afdsf

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -1,10 +1,10 @@
 import pytest
 
 class Test_regression:
-    def test_01(self):
+    def test_01():
         print("+++++++++++++++++++++++++++++++++++")
 
-    def test_02(self):
+    def test_02():
         assert 5 == 5
 
 # if __name__ == '__main__':
