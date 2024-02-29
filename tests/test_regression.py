78c13637b232e6b6bb26006eb3d9244709ed6e44
misha <mishafrancis@gmail.com>
2024-03-01 00:13:24 +0100

    ZXc

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -7,8 +7,9 @@ class Test_regression:
 
     @pytest.mark.smoke
     def test_02(self):
-        assert 5 == 5
+
         print("=========333==d=d=======")
+        assert 5 == 51
 
 # if __name__ == '__main__':
 #     Test_regression.test_01()
