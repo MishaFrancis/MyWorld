3950e444a5e90700b740949e7f579f7b47613fbe
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-02-29 12:33:31 +0100

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,2 @@
-import pytest
-
-class test_regression:
-    @pytest.mark.smoke
-    def test_01():
-        print("++++++++++++++++++++++++++++++++++++++++++++++++")
-        assert 1 + 1 == 2
-
-
-if __name__ == '__main__':
-    test_regression.test_01()
+def test_main():
+    assert "a string value" == "a string value"
