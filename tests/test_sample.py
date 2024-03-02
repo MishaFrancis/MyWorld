bc43cd84e7f03966eb1956dbed6275c429e1de19
Misha Francis <160722034+MishaFrancis@users.noreply.github.com>
2024-03-03 00:09:51 +0100

    zcx

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -3,7 +3,7 @@ import pytest
 class tests_regressions:
 
     @pytest.mark.smoke
-    def test_01_check():
+    def test_01_check(self):
 
         print("++++++++++++++++++++++++++++++++++++++++++++++++")
         assert 1 + 1 == 2
