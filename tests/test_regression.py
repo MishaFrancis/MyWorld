8b844dc26176991319688ec588321ead46c67f43
MishaFrancis <160722034+MishaFrancis@users.noreply.github.com>
2024-03-02 16:19:56 +0100

    asdfs

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -2,14 +2,22 @@ import pytest
 
 class Test_regression:
     @pytest.mark.smoke
-    def test_01(self):
+    def test_01_Simple_print_output_check(self):
+
         print("+++++++++++++++++++++++++++++++++++")
 
     @pytest.mark.smoke
-    def test_02(self):
+    def test_02_simple_assert_check(self):
 
-        print("=========333==d=d=======")
         assert 5 == 5
+
+    @pytest.mark.smoke
+    def test_03_simple_url_print_check(self):
+
+        url = 'www.google.com'
+        print (url)
+
+
 
 # if __name__ == '__main__':
 #     Test_regression.test_01()
