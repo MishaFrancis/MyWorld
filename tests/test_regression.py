97a9828d8d6dc98f2a80093109bab490edfa9bbb
Misha Francis <160722034+MishaFrancis@users.noreply.github.com>
2024-03-03 18:21:35 +0100

    ddd

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -75,7 +75,7 @@ class Test_regression:
         # conn.close()
 
         cursor = conn.cursor()
-        query = 'INSERT INTO users VALUE ("4","john",CURTIME());'
+        query = 'INSERT INTO users VALUE ("1","john",CURTIME());'
         cursor.execute(query)
         query = 'SELECT * FROM users'
         cursor.execute(query)
