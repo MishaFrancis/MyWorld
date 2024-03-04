daf64c509c745c4dbc6321b0a27dcd6bd524e8fe
Misha Francis <160722034+MishaFrancis@users.noreply.github.com>
2024-03-05 00:23:51 +0100

    sdf

--- tests/test_regression.py
+++ tests/test_regression.py
@@ -57,8 +57,8 @@ class Test_regression:
 
         print('In Progress with API')
 
-        response = requests.post("https://dummy.restapiexample.com/api/v1/create", 
-                   data={"name":"test","salary":"123","age":"23"},
+        response = requests.post("https://httpbin.org/post", 
+                   data={"key": "value"},
                    headers={"Content-Type": "application/json"},
                    )
 
